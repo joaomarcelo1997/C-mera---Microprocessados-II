b31062e2b0b4d9b51cb083205b6351e2f8ff1c59
joaomarcelo1997 <joaomarcelogcc@gmail.com>
2016-08-22 12:27:41 -0300

    Create adc_confg.h

---
+++ adc_confg.h
@@ -0,0 +1,17 @@
+/*
+ * adc_confg.h
+ *
+ *  Created on: 16 de ago de 2016
+ *      Author: Juliana
+ */
+
+#ifndef ADC_CONFG_H_
+#define ADC_CONFG_H_
+
+#define conversao 0x18
+
+void adc_confg();
+
+
+
+#endif /* ADC_CONFG_H_ */
